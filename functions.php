54a19ee917950eb82160d72013a0470be1c18ef3
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-03-18 11:58:06 +0100

    Legger til landsfestivalen.css på DOM

--- functions.php
+++ functions.php
@@ -9,10 +9,15 @@ function your_child_theme_enqueue_styles() {
 add_action('wp_enqueue_scripts', 'your_child_theme_enqueue_styles');
 
 
+function enqueue_styles() {
+    wp_enqueue_style('UKMFestivalenSideVueStyle', '/wp-content/themes/astra-child-theme/styles/landsfestivalen.css');
+}
+add_action('wp_enqueue_scripts', 'enqueue_styles');
+
 
 // [landsfestivalen_shortcode component="deltakere"]
 function vue_component_shortcode($atts) {
-    enqueue_api_script();
+    enqueue_shortcodes_script();
 
     $atts = shortcode_atts(['component' => 'DefaultComponent'], $atts);
     return '<div class="vue-app" data-vue-component="' . esc_attr($atts['component']) . '"></div>';
@@ -21,7 +26,7 @@ add_shortcode('landsfestivalen_shortcode', 'vue_component_shortcode');
 
 
 
-function enqueue_api_script() {
+function enqueue_shortcodes_script() {
     wp_enqueue_style('UKMFestivalenSideBootstrap4', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css');
 
     wp_enqueue_style('UKMFestivalenSideVueStyle', '/wp-content/themes/astra-child-theme/client/dist/assets/build.css');
