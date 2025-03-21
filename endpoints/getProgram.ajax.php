7dd80cb788014e61dd0f25152aacc4c01ff08e6c
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-03-21 15:17:18 +0100

    Ny endpoint for å hente hendelser for program

---
+++ endpoints/getProgram.ajax.php
@@ -0,0 +1,27 @@
+<?php
+
+use UKMNorge\OAuth2\HandleAPICall;
+use UKMNorge\Geografi\Kommune;
+use UKMNorge\Arrangement\Arrangement;
+use UKMNorge\Arrangement\Program\Hendelser;
+use UKMNorge\Arrangement\UKMFestival;
+
+require_once('UKM/Autoloader.php');
+
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+
+$handleCall = new HandleAPICall([], ['visInterne'], ['GET', 'POST'], false);
+
+$visInterne = $handleCall->getOptionalArgument('visInterne') == true ?? false;
+
+$arrangement = UKMFestival::getCurrentUKMFestival();
+
+
+$hendelser = $visInterne ? $arrangement->getProgram()->getAllInkludertInterne() : $arrangement->getProgram()->getAll();
+// $program = Hendelser::sorterPerDag( $hendelser );
+
+
+$handleCall->sendToClient([
+    'hendelser' => $hendelser,
+]);
