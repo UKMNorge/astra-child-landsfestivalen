a38d4ad7dc0fbabb4f1051ce121f0f011ccb3af9
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-03-24 16:24:25 +0100

    Dato forbedringer og tomme verdier

--- client/src/objects/Hendelse.ts
+++ client/src/objects/Hendelse.ts
@@ -21,12 +21,17 @@ class Hendelse {
         this.beskrivelse = beskrivelse;
     }
 
-    getStart() : string {
-        // convert timestamp to date string
+    getStart(): string {
+        // Convert timestamp to date
         const startDate = new Date(this.start * 1000);
         
-        return `${String(startDate.getDate()).padStart(2, '0')}.${String(startDate.getMonth() + 1).padStart(2, '0')} ${String(startDate.getHours()).padStart(2, '0')}:${String(startDate.getMinutes()).padStart(2, '0')}`
+        // Days of the week array
+        const daysOfWeek = ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'];
+    
+        // Format the date to the desired format "Mandag hh:mm"
+        return `${daysOfWeek[startDate.getDay()]} ${String(startDate.getHours()).padStart(2, '0')}:${String(startDate.getMinutes()).padStart(2, '0')}`;
     }
+    
 }
 
 
