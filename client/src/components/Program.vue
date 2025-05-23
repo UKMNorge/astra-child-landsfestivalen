ca380d14d4ec3d2697f6c885668f844a13c99b79
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-05-23 14:13:02 +0200

    Søk innslag

--- client/src/components/Program.vue
+++ client/src/components/Program.vue
@@ -49,6 +49,21 @@
             </div>
         </div>
 
+        <div v-show="getFoundInnslag().length > 0" class="deltakere-found">
+            <h6 class="title">Innslag: </h6>
+            <div class="as-margin-bottom-space-2">
+                <v-chip @click="openSingleHendelse(innslag)" v-for="innslag in getFoundInnslag()" :key="innslag.id" class="deltaker-result-chip as-margin-right-space-1">
+                    <span class="found-deltaker-inside-chip">
+                        <span>
+                            <svg class="deltaker-icon" width="14" height="15" viewBox="0 0 24 24" fill="#fff" xmlns="http://www.w3.org/2000/svg">
+                                <title>calendar-heart</title><path d="M19 19V8H5V19H19M16 1H18V3H19C20.11 3 21 3.9 21 5V19C21 20.11 20.11 21 19 21H5C3.9 21 3 20.11 3 19V5C3 3.9 3.9 3 5 3H6V1H8V3H16V1M12 17.17L11.42 16.64C9.36 14.77 8 13.54 8 12.03C8 10.8 8.97 9.83 10.2 9.83C10.9 9.83 11.56 10.15 12 10.66C12.44 10.15 13.1 9.83 13.8 9.83C15.03 9.83 16 10.8 16 12.03C16 13.54 14.64 14.77 12.58 16.64L12 17.17Z" />
+                            </svg>
+                        </span>
+                        <b>{{ innslag.navn }}</b></span>
+                </v-chip>
+            </div>
+        </div>
+
         <template v-for="hendelse in getFilteredHendelser()" :key="hendelse.id">
             <div class="hendelse">
                 <div @click="hendelse.isOpen = !hendelse.isOpen" class="first-width">
@@ -223,6 +238,7 @@ export default {
 
             hendelseMedAktiviteter : {} as any,
             deltakere : {} as any,
+            alleInnslag : {} as any,
 
         };
     },
@@ -249,6 +265,35 @@ export default {
             }
 
             return false;
+        },
+        getFoundInnslag() : any[] {
+            let filtered = Object.values(this.alleInnslag);
+            console.log('filtered innslag');
+            console.log(filtered);
+
+            if (this.searchWords && this.searchWords.length > 2) {
+                const fuse = new Fuse(filtered, {
+                    threshold: 0.0, // exact match only
+                    keys: ['navn'],
+                    useExtendedSearch: true,
+                    getFn: (deltaker: any, path: string | string[]) => {
+                        const key = Array.isArray(path) ? path[0] : path;
+                        switch (key) {
+                            case 'navn':
+                                return deltaker.navn;
+                            default:
+                                return '';
+                        }
+                    }
+                });
+
+                // Use exact match operator with '
+                const results = fuse.search(`'${this.searchWords}`);
+                return results.slice(0, 5).map(r => r.item);
+            } else {
+                return [];
+            }
+
         },
         getFoundDeltakere() : any[] {
             let filtered = Object.values(this.deltakere);
@@ -384,7 +429,8 @@ export default {
                             this.deltakere[person.id +'_'+ person.context.forestilling.id+ '_' + person.innslagId] = person;
                             deltakereNavn.push(person.fornavn + ' ' + person.etternavn);
                         }
-                    }   
+                    }
+                    this.alleInnslag[innslag.id] = innslag;
                 }
 
                 var newHendelse = new Hendelse(
@@ -402,8 +448,8 @@ export default {
                 );
                 this.hendelser.push(newHendelse);
                 
-                console.log('alle deltakere');
-                console.log(this.deltakere);
+                console.log('alle innslag');
+                console.log(this.alleInnslag);
 
                 if(this.availableTider.find(t => t.id == newHendelse.getStartDag()) == undefined && h.start && h.start.trim() != '') {
                     this.availableTider.push({'id' : newHendelse.getStartDag(), 'title' : newHendelse.getStartDag()});
